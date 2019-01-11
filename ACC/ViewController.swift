bcd4fb0c6130415c8b96a0e990d6d9623654494e
Leonardo Araque <leo@Leonardos-MacBook-Pro.local>
2019-01-11 14:43:21 -0500

    Update ViewController.swift

--- ACC/ViewController.swift
+++ ACC/ViewController.swift
@@ -1,20 +1,59 @@
 //
 //  ViewController.swift
-//  ACC
 //
-//  Created by Leonardo Araque on 1/11/19.
-//  Copyright © 2019 Leonardo Araque. All rights reserved.
+//
+//  Created by Leonardo Araque on 11/22/18.
+//  Copyright © 2018 Leonardo Araque. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    struct messageStruct : Codable{
+        var userId : Int
+        var id : Int
+        var title : String
+        var completed : Bool
+    }
+    
+    @IBOutlet weak var imageContainer: UIImageView!
+    
+    
+    @IBOutlet weak var displayJSON: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let imageURL = URL(string: "https://www.ucf.edu/brand/files/2016/07/UCF-Tab-Signature-lockup_vertical-KG-7406.png")!
+        let task = URLSession.shared.dataTask(with: imageURL) { (data, response, error) in
+            if (error == nil){
+                
+                let loadedImage = UIImage(data: data!)
+                DispatchQueue.main.async {
+                    self.imageContainer.image = loadedImage
+                }
+            }
+        }
+        task.resume()
+        
+        var request = URLRequest(url: URL(string: "http://127.0.0.1:8000")!)
+        request.httpMethod = "GET"
+        URLSession.shared.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            do {
+                let jsonDecoder = JSONDecoder()
+                let responseModel = try jsonDecoder.decode(messageStruct.self, from: data!)
+                print(responseModel)
+                DispatchQueue.main.async {
+                    self.displayJSON.text = responseModel.title
+                    self.displayJSON.sizeToFit()
+                }
+            } catch {
+                print("JSON Serialization error")
+            }
+        }).resume()
     }
-
-
 }
+
 
