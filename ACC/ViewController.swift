6385f423f6064b8ac797b59db724f629476a0f4c
Leonardo Araque <leo@leonardos-mbp>
2019-01-20 19:51:46 -0500

    Main storyboard created with login fields that refer to a new state/storyboard if successful. HTTP GET, HTTP POST, User Preferences, and View transitions are working.

--- ACC/ViewController.swift
+++ ACC/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     struct messageStruct : Codable{
         var userId : Int
@@ -17,14 +17,35 @@ class ViewController: UIViewController {
         var completed : Bool
     }
     
+    struct usernamePass : Codable {
+        var email: String
+        var password: String
+    }
+    
+    struct loginReturn : Codable{
+        var token: String
+        var email: String
+        var username: String
+    }
+    
     @IBOutlet weak var imageContainer: UIImageView!
+    @IBOutlet weak var displayText: UILabel!
     
+    @IBOutlet weak var inputPassword: UITextField!
     
-    @IBOutlet weak var displayJSON: UILabel!
-    
+    @IBAction func inputProcess(_ sender: UIButton) {
+        login()
+        print("ran")
+    }
+    @IBOutlet weak var inputEmail: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        inputPassword.delegate = self
+        inputEmail.delegate = self
+        
+        
         
         let imageURL = URL(string: "https://www.ucf.edu/brand/files/2016/07/UCF-Tab-Signature-lockup_vertical-KG-7406.png")!
         let task = URLSession.shared.dataTask(with: imageURL) { (data, response, error) in
@@ -38,22 +59,54 @@ class ViewController: UIViewController {
         }
         task.resume()
         
-        var request = URLRequest(url: URL(string: "http://127.0.0.1:8000")!)
-        request.httpMethod = "GET"
+        
+
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    func login(){
+        var request = URLRequest(url: URL(string: "http://127.0.0.1:8000/users/")!)
+        request.httpMethod = "POST"
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")  // the request is JSON
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Accept")
+        //let inpuT = usernamePass(email: "tuanvpham32@outlook.com", password: "Pviett#8613")
+        let inpuT = usernamePass(email: inputEmail.text! as String, password: inputPassword.text! as String)
+        //print(inpuT)
+        //print(input2)
+        
+        let encoder = JSONEncoder();
+        request.httpBody = try! encoder.encode(inpuT)
+        
         URLSession.shared.dataTask(with: request, completionHandler: { data, response, error -> Void in
-            do {
-                let jsonDecoder = JSONDecoder()
-                let responseModel = try jsonDecoder.decode(messageStruct.self, from: data!)
-                print(responseModel)
+            do{
+                let decoder = JSONDecoder()
+                let responseJson = try decoder.decode(loginReturn.self, from: data!)
                 DispatchQueue.main.async {
-                    self.displayJSON.text = responseModel.title
-                    self.displayJSON.sizeToFit()
+                    print(responseJson.email + "\n")
+                    print(responseJson.username + "\n")
+                    print(responseJson.token + "\n")
+                    let usrDefaults = UserDefaults.standard
+                    usrDefaults.set(responseJson.email, forKey: "email")
+                    usrDefaults.set(responseJson.username, forKey: "username")
+                    usrDefaults.set(responseJson.token, forKey: "token")
+                    
+                    let viewController:UIViewController = UIStoryboard(name: "Sessions", bundle: nil).instantiateViewController(withIdentifier: "WelcomeSession") as UIViewController
+                    
+                    self.present(viewController, animated: false, completion: nil)
                 }
-            } catch {
-                print("JSON Serialization error")
+                
+            } catch{
+                print(error)
             }
+            
         }).resume()
     }
-}
 
+}
 
---
+++ Sessions/WelcomeSessionViewController.swift
@@ -0,0 +1,39 @@
+//
+//  WelcomeSessionViewController.swift
+//  ACC
+//
+//  Created by Leonardo Araque on 1/20/19.
+//  Copyright © 2019 Leonardo Araque. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeSessionViewController: UIViewController {
+
+    @IBOutlet weak var displayEmail: UILabel!
+    
+    @IBOutlet weak var displayUsername: UILabel!
+    @IBOutlet weak var displayToken: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let data = UserDefaults.standard
+        displayEmail.text = data.string(forKey: "email")
+        displayUsername.text = data.string(forKey: "username")
+        displayToken.text = data.string(forKey: "token")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
